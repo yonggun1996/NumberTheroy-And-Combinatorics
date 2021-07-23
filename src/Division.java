f5f20c829d4785177d4b44db50a814023adb5cef
qkrdydrjs7@naver.com <qkrdydrjs7@naver.com>
2021-07-23 20:57:25 +0900

    백준 알고리즘 1037번 소스코드 입니다.

---
+++ src/Division.java
@@ -0,0 +1,30 @@
+package NumberTheroyAndCombinatorics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Division {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		int max = arr[n - 1];
+		int min = arr[0];
+		
+		System.out.println(max * min);
+		
+	}
+
+}
